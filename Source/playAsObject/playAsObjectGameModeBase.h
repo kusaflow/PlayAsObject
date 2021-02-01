90997482cb3bcd415ca3fce91bd33eb01b81cd1f
Kunal SAgar <kunalsagar1999@gmail.com>
2021-02-01 13:04:29 +0530

    init

---
+++ Source/playAsObject/playAsObjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "playAsObjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYASOBJECT_API AplayAsObjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
