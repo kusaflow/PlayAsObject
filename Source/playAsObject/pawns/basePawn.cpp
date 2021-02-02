cf38b63ffd56311e431404dd08345d82e0fd996b
Kunal SAgar <kunalsagar1999@gmail.com>
2021-02-03 00:47:58 +0530

    cpp

---
+++ Source/playAsObject/pawns/basePawn.cpp
@@ -0,0 +1,100 @@
+ // Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "basePawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+
+
+// Sets default values
+AbasePawn::AbasePawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("meshComp"));
+	RootComponent = mesh;
+
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	cameraBoom->SetupAttachment(GetRootComponent());
+	cameraBoom->bDoCollisionTest = false;
+	cameraBoom->TargetArmLength = 800;
+	cameraBoom->SocketOffset = FVector(0.f, 0.f, 0.f);
+	//cameraBoom->SetRelativeRotation(FRotator(-20, -70, 0.f));
+	//cameraBoom->RelativeRotation = FRotator(0.f, 0.f, 0.f);
+	//cameraBoom->bUsePawnControlRotation = false;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(cameraBoom, USpringArmComponent::SocketName);
+
+
+}
+
+// Called when the game starts or when spawned
+void AbasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	mesh->SetSimulatePhysics(true);
+	
+}
+
+// Called every frame
+void AbasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	//Line Trace
+	FHitResult OutHit;
+	FVector Start = FollowCamera->GetComponentLocation();
+
+	// alternatively you can get the camera location
+	// FVector Start = FirstPersonCameraComponent->GetComponentLocation();
+
+	
+	FVector End = ((FollowCamera->GetForwardVector() * 1000.f) + Start);
+	FCollisionQueryParams CollisionParams;
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 0.1f, 0, 1);
+
+	if ()
+	if (GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, CollisionParams)) {
+
+	}
+
+
+
+}
+
+// Called to bind functionality to input
+void AbasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("xAxis", this, &AbasePawn::CameraYaw_z);
+	PlayerInputComponent->BindAxis("yAxis", this, &AbasePawn::CameraPitch_y);
+
+
+}
+
+
+void AbasePawn::CameraYaw_z(float val) {
+	FRotator newR = FRotator(0, 0, 0);
+	newR.Yaw += val;
+	cameraBoom->AddRelativeRotation(newR);
+
+}
+
+void AbasePawn::CameraPitch_y(float val) {
+	
+	FRotator newR = cameraBoom->GetRelativeRotation();
+	newR.Pitch = FMath::Clamp(newR.Pitch + val, -80.0f, 4.0f);
+
+	cameraBoom->SetRelativeRotation(newR);
+}
+
+
+
+
